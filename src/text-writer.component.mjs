78d39df28c0a7c3c87faa4198e5a3fd8e359e928
David Blanchard <dpjb@hotmail.fr>
2023-05-21 09:58:42 +0200

    Lint the project

--- src/text-writer.component.mjs
+++ src/text-writer.component.mjs
@@ -1,68 +1,58 @@
-import TextWriter from "./text-writer.class.mjs"
+/* eslint-disable import/extensions */
+import TextWriter from "./text-writer.class.mjs";
 
 export default class TextWriterComponent extends HTMLElement {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
-
-        this.text
-        this.attachShadow({mode: 'open'})
-        this.shadowRoot.innerHTML = `
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
 <div class="text-snippet">
     <div class="to-be-written">
         <div id="to-write"></div>
     </div>
 </div>
-`
+`;
+  }
 
-        const slots = this.shadowRoot.querySelectorAll('slot')
-        if (slots.length) {
-            slots[0].addEventListener('slotchange', event => {
-                console.dir(slots[0].assignedNodes())
-            })
-        }
-
-    }
-
-    static get observeAttributes() {
-        /**
+  static get observeAttributes() {
+    /**
          * Attributes passed inline to the component
          */
-        return ['text']
-    }
+    return ["text"];
+  }
 
-    get text() {
-        return this.getAttribute('text') ?? null
-    }
+  get text() {
+    return this.getAttribute("text") ?? null;
+  }
 
-    get styles() {
-        return this.getAttribute('styles') ?? null
-    }
+  get styles() {
+    return this.getAttribute("styles") ?? null;
+  }
 
-    get classes() {
-        return this.getAttribute('classes') ?? null
-    }
+  get classes() {
+    return this.getAttribute("classes") ?? null;
+  }
 
-    async connectedCallback() {
-        /**
+  async connectedCallback() {
+    /**
          * The magic starts here
          */
-        if (this.styles !== null && this.classes !== null) {
-            const $styleList = this.styles.split(',')
+    if (this.styles !== null && this.classes !== null) {
+      const $styleList = this.styles.split(",");
 
-            $styleList.forEach($item => {
-                const style = document.createElement('style')
-                style.innerHTML = `@import "${$item}"`
+      $styleList.forEach(($item) => {
+        const style = document.createElement("style");
+        style.innerHTML = `@import "${$item}"`;
 
-                this.shadowRoot.appendChild(style)
-            })
+        this.shadowRoot.appendChild(style);
+      });
 
-            const parentDiv = this.shadowRoot.getElementById('to-write')
-            parentDiv.setAttribute('class', this.classes)
-        }
-
-        const base = new TextWriter(this)
-        base.writeLikeAHuman('to-write')
-
+      const parentDiv = this.shadowRoot.getElementById("to-write");
+      parentDiv.setAttribute("class", this.classes);
     }
+
+    const base = new TextWriter(this);
+    base.writeLikeAHuman("to-write");
+  }
 }
