253772419b6487b7a74d5f681760c5117367ed28
David Blanchard <david.blanchard71@icloud.com>
2023-06-11 22:02:40 +0200

    Prettify

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import CodeWriterComponent from "./src/code-writer.component.js";
 import TextWriterComponent from "./src/text-writer.component.js";
 
 export default function index() {
-  customElements.define("code-writer", CodeWriterComponent);
-  customElements.define("text-writer", TextWriterComponent);
+    customElements.define("code-writer", CodeWriterComponent);
+    customElements.define("text-writer", TextWriterComponent);
 }
 
 index();
