2595f5b0bfec91b39c10ad6b32156565dc1f14ad
David Blanchard <david.blanchard71@icloud.com>
2023-10-07 23:41:58 +0200

    Fix snippet-width assignment

--- web/components/code-writer.component.js
+++ web/components/code-writer.component.js
@@ -1,41 +1,43 @@
-import { Writer } from "../../core/writer.js";
+import {Writer} from "../../core/writer.js";
 import WriterComponent from "./writer-component.js";
 
 export default class CodeWriterComponent extends WriterComponent {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.attachShadow({
-            mode: "open"
-        });
-        this.shadowRoot.innerHTML = `
+    this.attachShadow({
+      mode: "open"
+    });
+    this.shadowRoot.innerHTML = `
 <style>
 .code-snippet {
     display: flex;
-    font-size: medium;
+    font-size: small;
+    width: ${this.snippetWidth};
 }
 
 .code-snippet .to-be-placed {
     display: block;
     position: relative;
     float: left;
+    width: inherit;
 }
 
 .code-snippet .to-place {
     display: block;
     position: relative;
     float: left;
-    width: ${this.snippetWidth};
+    width: fit-content;
 }
 
 .code-snippet .to-be-written {
     display: flex;
     position: absolute;
-    width: ${this.snippetWidth};
+    width: inherit;
 }
 
 .code-snippet .to-write {
-    width: ${this.snippetWidth};
+    width: fit-content;
 }
 
 .code-snippet div, p, span, textarea {
@@ -55,100 +57,100 @@ export default class CodeWriterComponent extends WriterComponent {
     </div>
 </div>
 `;
+  }
+
+  static get observeAttributes() {
+    /**
+     * Attributes passed inline to the component
+     */
+    const parentAttributes = super.observeAttributes();
+
+    return Array.prototype.concat(parentAttributes, [
+      "use-highlight-js",
+      "theme",
+      "language",
+      "snippet-width"
+    ]);
+  }
+
+  get useHighlightJs() {
+    const result = this.getAttribute("use-highlight-js") ?? "";
+    return result.toLowerCase() === "true";
+  }
+
+  get theme() {
+    return this.getAttribute("theme") ?? null;
+  }
+
+  get language() {
+    return this.getAttribute("language") ?? null;
+  }
+
+  get snippetWidth() {
+    return this.getAttribute("snippet-width") ?? "45vw";
+  }
+
+  async connectedCallback() {
+    /**
+     * Integrate highlightJs
+     */
+
+    if (this.useHighlightJs) {
+      const $theme = this.theme ?? "base16/monokai";
+      const $language = this.language ?? "html";
+
+      const script = document.createElement("script");
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js";
+      this.shadowRoot.appendChild(script);
+
+      const $styleList = [];
+      $styleList.push(
+        "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css"
+      );
+      $styleList.push(
+        `https://highlightjs.org/static/demo/styles/${$theme}.css`
+      );
+
+      $styleList.forEach(($item) => {
+        const style = document.createElement("style");
+        style.innerHTML = `@import "${$item}"`;
+
+        this.shadowRoot.appendChild(style);
+      });
+
+      const $parentDiv = this.shadowRoot.querySelectorAll("code");
+      for (const node of $parentDiv) {
+        node.setAttribute("class", `language-${$language}`);
+      }
     }
 
-    static get observeAttributes() {
-        /**
-         * Attributes passed inline to the component
-         */
-        const parentAttributes = super.observeAttributes();
+    await super.connectedCallback();
+  }
 
-        return Array.prototype.concat(parentAttributes, [
-            "use-highlight-js",
-            "theme",
-            "language",
-            "snippet-width"
-        ]);
-    }
+  onFinishedWriting(html) {
+    // Raise an event outside the shadow DOM
+    // when all is done and ready
+    const finishedEvent = new CustomEvent("finishedWriting", {
+      bubbles: true,
+      composed: true,
+      detail: {
+        content: html
+      }
+    });
+    this.dispatchEvent(finishedEvent);
+    this.setAttribute("finished", "true");
+  }
 
-    get useHighlightJs() {
-        const result = this.getAttribute("use-highlight-js") ?? "";
-        return result.toLowerCase() === "true";
-    }
-
-    get theme() {
-        return this.getAttribute("theme") ?? null;
-    }
-
-    get language() {
-        return this.getAttribute("language") ?? null;
-    }
-
-    get snippetWidth() {
-        return this.getAttribute("snippet-width") ?? "45vw";
-    }
-
-    async connectedCallback() {
-        /**
-         * Integrate highlightJs
-         */
-
-        if (this.useHighlightJs) {
-            const $theme = this.theme ?? "base16/monokai";
-            const $language = this.language ?? "html";
-
-            const script = document.createElement("script");
-            script.src =
-                "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js";
-            this.shadowRoot.appendChild(script);
-
-            const $styleList = [];
-            $styleList.push(
-                "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css"
-            );
-            $styleList.push(
-                `https://highlightjs.org/static/demo/styles/${$theme}.css`
-            );
-
-            $styleList.forEach(($item) => {
-                const style = document.createElement("style");
-                style.innerHTML = `@import "${$item}"`;
-
-                this.shadowRoot.appendChild(style);
-            });
-
-            const $parentDiv = this.shadowRoot.querySelectorAll("code");
-            for (const node of $parentDiv) {
-                node.setAttribute("class", `language-${$language}`);
-            }
-        }
-
-        await super.connectedCallback();
-    }
-
-    onFinishedWriting(html) {
-        // Raise an event outside the shadow DOM
-        // when all is done and ready
-        const finishedEvent = new CustomEvent("finishedWriting", {
-            bubbles: true,
-            composed: true,
-            detail: {
-                content: html
-            }
-        });
-        this.dispatchEvent(finishedEvent);
-        this.setAttribute("finished", "true");
-    }
-
-    async writeLikeAHuman() {
-        const onFinished = this.onFinishedWriting.bind(this);
-        const cw = new Writer(
-            this.shadowRoot,
-            this.source,
-            this.speed,
-            this.makeTypos,
-            onFinished
-        );
-        await cw.writeLikeAHuman("pre.to-write code", "pre.to-place code");
-    }
+  async writeLikeAHuman() {
+    const onFinished = this.onFinishedWriting.bind(this);
+    const cw = new Writer(
+      this.shadowRoot,
+      this.source,
+      this.speed,
+      this.makeTypos,
+      onFinished
+    );
+    await cw.writeLikeAHuman("pre.to-write code", "pre.to-place code");
+  }
 }
