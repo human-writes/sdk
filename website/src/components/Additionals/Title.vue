b3f49ca2398dfdc6e914f90b0762d252d8ba6bb4
David Blanchard <david.blanchard71@icloud.com>
2024-01-20 14:35:48 +0100

    Fix the website header.

--- website/src/components/Additionals/Title.vue
+++ website/src/components/Additionals/Title.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg id="title" viewBox="0 0 400 65" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+  <svg id="title" viewBox="0 0 400 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
     <text class="deep-purple" x="5" y="48">Human Writes</text>
   </svg>
 </template>
