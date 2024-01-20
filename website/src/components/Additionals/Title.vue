e97e09c00fbe01aa5e2bc7ba5f719609f34a518e
David Blanchard <david.blanchard71@icloud.com>
2024-01-20 19:55:57 +0100

    WebSite now fits to mobile responsive design.

--- website/src/components/Additionals/Title.vue
+++ website/src/components/Additionals/Title.vue
@@ -1,6 +1,6 @@
 <template>
-  <svg id="title" viewBox="0 0 400 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
-    <text class="deep-purple" x="5" y="48">Human Writes</text>
+  <svg id="title" viewBox="0 0 311 48" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+    <text class="deep-purple" x="5" y="36">Human Writes</text>
   </svg>
 </template>
 <script lang="ts" setup></script>
@@ -8,7 +8,7 @@
 .deep-purple {
   font-family: 'Orbitron', Arial, Helvetica, sans-serif;
   font-weight: 700;
-  font-size: 48px;
+  font-size: 36px;
   fill: var(--inter-fill-color);
 }
 </style>
